5d085e70edcfd5438cbfd0bbb7d9f55447709a40
ers339444-lgtm <ers339444@gmail.com>
2026-01-20 15:11:03 -0600

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ const gameText= document.getElementById('game-text2');
 const userInput= document.getElementById('user-input2');
 const submitBtn= document.getElementById('submit-btn2');
 function print(text) {
-    gameText.innerText +=text + "\n";
+    gameText.innerText += text + "\n";
     gameText.scrollTop= gameText.scrollHeight;
 }
 function sleep(ms){
@@ -10,17 +10,17 @@ function sleep(ms){
 }
 let currentStep= "askUsername";
 let username = "";
-submitBtn.addEventListner("click", async () =>{
+submitBtn.addEventListener("click", async () =>{
   const input = userInput.value.trim();
   userInput.value = "";
   if (currentStep === "askUsername"){
     username=input; 
-    if (username==="student" || username==="teacher"){
+    if (username ==="student" || username==="teacher"){
       print("Thank You");
       await sleep (1000);
       print("Please input your password:");
       currentStep = "askPassword";
-    } else{
+    } else {
       print("Incorrect username. Please refresh page and try again.");
       currentStep="end";
     }
@@ -29,10 +29,10 @@ submitBtn.addEventListner("click", async () =>{
       if(username==="student" && password1==="123456"){
           print("You are logged in as a student.");
           document.getElementById("main-link").style.display="block";
-      } else if (username==="teacher" && password1==="querty"){
-          print (You are logged in as a teacher");
-           document.getElementById("main-link").style.display="block";
-      } else{
+      } else if (username==="teacher" && password1==="qwerty"){
+            print (You are logged in as a teacher");
+            document.getElementById("main-link").style.display="block";
+      } else {
           print("incorrect password");
           await sleep(1500);
           print("Refresh page and try again");
@@ -43,4 +43,3 @@ submitBtn.addEventListner("click", async () =>{
       }
   });
 print("Log-in Page \n\nWelcome! Please enter your username:");
-      
