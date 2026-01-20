a6fc0f6cae451b06612acf00f1dac9dae84eba22
ers339444-lgtm <ers339444@gmail.com>
2026-01-20 14:09:09 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
-const gameText= document.getElementByld("game-text2");
-const userInput= document.getElementByld("user-input2");
-const submitBtn= document.getElementByld("submit-btn2");
+const gameText= document.getElementByld('game-text2');
+const userInput= document.getElementByld('user-input2');
+const submitBtn= document.getElementByld('submit-btn2');
 function print(text) {
     gameText.innerText +=text + "\n";
     gameText.scrollTop= gameText.scrollHeight;
